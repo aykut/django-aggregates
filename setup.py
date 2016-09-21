23fea67a120f1e010109faa16f0bc5e8382cc1b9
Aykut Özat <aykutozat@gmail.com>
2016-09-21 22:06:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-aggregates',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     description='Aggregate, String and Conditional SQL functions for '
